05b00a382fe2805c79be50a77a635774e9622ed7
Gonzalo <gonxadev@gmail.com>
2026-02-09 19:52:05 +0100

    clean main

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main(int argc, char** argv) {
+    std::cout << "Arrancando proyecto 'noobie'...\n";
+
+    // TODO: Inicializa aquí tu motor/servicio. Ejemplo:
+    // MotorAjedrez motor;
+    // motor.Iniciar();
+
+    std::cout << "Inicialización por defecto completada (sin motor).\n";
+    return 0;
+}
