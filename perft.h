7123392985fefd3bb452b317c36bebbc797344e1
Gonzalo <gonxadev@gmail.com>
2026-02-20 13:20:53 +0100

    IndexToSquare

--- perft.h
+++ perft.h
@@ -4,21 +4,17 @@
 class benchmark {
 public:
 	static uint64_t perft(Board& board, int depth) {
-		MoveGenerator moveGen;
         if (depth == 0)
             return 1ULL;
 
         uint64_t total_nodes = 0;
 		int color = board.isWhiteToMove() ? Board::WHITE : Board::BLACK;
-        std::vector<Move> moves = moveGen.generateMoves(board, color); // pseudolegales
+        std::vector<Move> moves = board.generateLegalMoves(color); // pseudolegales
 
         for (const Move& move : moves) {
-
-            if (!board.isLegal(board, move, color))
-                continue;
-
             board.makeMove(move);
 			uint64_t nodes = perft(board, depth - 1);
+
             total_nodes += nodes;
             board.unmakeMove();
         }
@@ -32,21 +28,25 @@ public:
 
         uint64_t total_nodes = 0;
         int color = board.isWhiteToMove() ? Board::WHITE : Board::BLACK;
-        std::vector<Move> moves = moveGen.generateMoves(board, color); // pseudolegales
+        std::vector<Move> moves = board.generateLegalMoves(color); // pseudolegales
         
         for (const Move& move : moves) {
-            
-            if (!board.isLegal(board, move, color))
-                continue;
-            
             board.makeMove(move);
             uint64_t nodes = perft(board, depth - 1);
             
-            std::cout << "{ From: " << move.from << " To: " << move.to << " Piece: " << move.piece << " Nodes: " << nodes << " Depth: " << depth << " }" << std::endl;
+            std::cout << indexToSquare(move.from) << indexToSquare(move.to) << ": " << nodes << std::endl;
             
             total_nodes += nodes;
             board.unmakeMove();  
         }
         return total_nodes;
 	}
+    static std::string indexToSquare(int index) {
+        int file = index % 8;
+        int rank = index / 8;
+        std::string result;
+        result += static_cast<char>('a' + file);
+        result += static_cast<char>('1' + rank);
+        return result;
+    }
 };
